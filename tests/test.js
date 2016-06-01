96eec5d49df9463a5b9d698d3d5549d69293c998
Austin Kurpuis <austin@austinkurpuis.com>
2016-06-01 12:39:13 -0700

    Added some basic unit tests

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,32 @@
 (function(expect, describe, it, window) {
-    var container = $(window.__html__['tagger.html']);
+    var container = $(window.__html__['tests/tagger.html']);
     var tagger;
     
-    describe('should init', function(){
+    describe('.tagger() ', function(){
+        
         it('should init tagger', function(){
             tagger = container.find('#Tags').tagger();
-            tagger.add('test');
         });
         
         it('should have a tag', function(){
-            console.log(tagger);
             expect(tagger.getValues()).to.contain('starting');
         });
+        
+        it('should remove a tag', function(){
+            tagger.removeValue('starting');
+            expect(tagger.getValues()).to.have.lengthOf(0);
+        });
+        
+        it('should add a tags', function(){
+            tagger.add('foo');
+            expect(tagger.getValues()).to.contain('foo');
+        });
+
+        it('should add multiple tags', function(){
+            tagger.add(['bar', 'baz']);
+            expect(tagger.getValues()).to.eql(['foo', 'bar', 'baz']);
+        });
+        
     });
     
 })(chai.expect, describe, it, this);
