95329829bc015e9632160d13e2f2458ad8801150
imaustink <austin@austinkurpuis.com>
2016-12-15 12:02:21 -0800

    Updated tests

--- test/test.js
+++ test/test.js
@@ -1,46 +1,46 @@
 (function(expect, describe, it, window) {
     var container = $(window.__html__['tests/tagger.html']);
-    var tagger;
+    var input;
     
     describe('.tagger() ', function(){
         
         it('should init tagger', function(){
-            tagger = container.find('#Tags').tagger();
+            input = container.find('#Tags').tagger();
         });
         
         it('should have a tag', function(){
-            expect(tagger.getValues()).to.contain('starting');
+            expect(input.tagger('getValues')).to.contain('starting');
         });
         
         it('should remove a tag', function(){
-            tagger.removeValue('starting');
-            expect(tagger.getValues()).to.have.lengthOf(0);
+            input.removeValue('starting');
+            expect(input.tagger('getValues')).to.have.lengthOf(0);
         });
         
         it('should add a tags', function(){
-            tagger.addTag('foo');
-            expect(tagger.getValues()).to.contain('foo');
+            input.addTag('foo');
+            expect(input.tagger('getValues')).to.contain('foo');
         });
 
         it('should add multiple tags', function(){
-            tagger.add(['bar', 'baz', 'qux']);
-            expect(tagger.getValues()).to.eql(['foo', 'bar', 'baz', 'qux']);
+            input.add(['bar', 'baz', 'qux']);
+            expect(input.tagger('getValues')).to.eql(['foo', 'bar', 'baz', 'qux']);
         });
         
         it('should remove baz by index', function(){
-            tagger.removeIndex(2);
-            expect(tagger.getValues()).to.not.include('baz');
+            input.removeIndex(2);
+            expect(input.tagger('getValues')).to.not.include('baz');
         });
         
         it('should remove the last tag', function(){
-            tagger.removeLast();
-            expect(tagger.exists('baz')).to.be.false;
+            input.removeLast();
+            expect(input.tagger('exits', 'baz')).to.be.false;
         });
         
         it('should remove all tags', function(){
-            var tags = tagger.siblings('.label');
-            tagger.remove(tags);
-            expect(tagger.getValues()).to.have.lengthOf(0);
+            var tags = input.siblings('.label');
+            input.tagger('remove', tags);
+            expect(input.tagger('getValues')).to.have.lengthOf(0);
         });
         
     });
